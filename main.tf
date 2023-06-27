863c47904d660a9691bf1e0dbe657525a33adcfc
Yuvraj Singh <yuvraj.singh@squareops.com>
2023-06-27 15:54:06 +0530

    update changes for nginx and cluster autoscalar for v1,26

--- main.tf
+++ main.tf
@@ -221,7 +221,7 @@ resource "helm_release" "internal_nginx" {
   count      = var.internal_ingress_nginx_enabled ? 1 : 0
   name       = "internal-ingress-nginx"
   chart      = "ingress-nginx"
-  version    = "4.1.4"
+  version    = "4.7.0"
   namespace  = "internal-ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
   values = [
--- variables.tf
+++ variables.tf
@@ -25,7 +25,7 @@ variable "cluster_autoscaler_enabled" {
 
 variable "cluster_autoscaler_chart_version" {
   description = "Version of the cluster autoscaler helm chart"
-  default     = "9.19.1"
+  default     = "9.29.0"
   type        = string
 }
 
@@ -109,7 +109,7 @@ variable "aws_load_balancer_version" {
 
 variable "ingress_nginx_version" {
   description = "Specify the version of the NGINX Ingress Controller"
-  default     = "4.1.4"
+  default     = "4.7.0"
   type        = string
 }
 
