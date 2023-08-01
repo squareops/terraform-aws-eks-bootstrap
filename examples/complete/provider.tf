f33ac219e1f7447fde9ac594c97b02216c0bba05
Rohit Singh <rohit.kumar@squareops.com>
2023-08-01 11:18:23 +0530

    Removed the unwanted links from the affected files

--- examples/complete/provider.tf
+++ examples/complete/provider.tf
@@ -6,11 +6,11 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = "prod-eks"
+  name = ""
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = "prod-eks"
+  name = ""
 }
 
 provider "kubernetes" {
